aa91cf2e028299d9d36b49715ffd6a4ac3d1e8d5
ChrisZZG <cristiangonzz@gmail.com>
2023-08-04 18:38:46 -0300

    Practica Formulario de Registro Personas

---
+++ src/main/java/logica/MVCp1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package logica;
+
+import igu.Pantalla;
+
+/**
+ *
+ * @author EQUIPO
+ */
+public class MVCp1 {
+
+    public static void main(String[] args) {
+       
+        Pantalla panta = new Pantalla();
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+    }
+}
